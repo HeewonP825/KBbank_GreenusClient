8b804255a86b24245d0d06d75f4f6e4d5be42d06
J-MU <alsdnrdl001@gmail.com>
2022-09-06 02:14:30 +0900

    마이페이지 api binding

--- lib/models/myPageData.dart
+++ lib/models/myPageData.dart
@@ -18,15 +18,16 @@ class MyPageData {
     required this.badge3,
   });
 }
+//TODO 뱃지가 NULL일때 처리해야함 DEFAULT값이 있어야할듯?
 
-List<MyPageData> myPageData = [
-  MyPageData(
-    name: '그리너',
-    profileImage: "https://images.unsplash.com/photo-1497211419994-14ae40a3c7a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    level: 'Lv',
-    levelNum: 3,
-    badge1: '1',
-    badge2: '2',
-    badge3: '3',
-  )
-];
+// List<MyPageData> myPageData = [
+//   MyPageData(
+//     name: '그리너',
+//     profileImage: "https://images.unsplash.com/photo-1497211419994-14ae40a3c7a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+//     level: 'Lv',
+//     levelNum: 3,
+//     badge1: '1',
+//     badge2: '2',
+//     badge3: '3',
+//   )
+// ];
--- lib/screens/mypage/myPageScreen.dart
+++ lib/screens/mypage/myPageScreen.dart
@@ -4,28 +4,86 @@ import 'package:kbbank_practice/models/myPageData.dart';
 import 'components/myPageHeader.dart';
 import '../../../theme.dart';
 import '../../../../models/myPageData.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class MyPageScreen extends StatelessWidget {
+Future<MyPageData> receiveMyPageData() async {
+  var userId = 12;
+
+  var jsonString = await http.get(
+      Uri.parse('https://dev.uksfirstdomain.shop/app/users/${userId}'));
+  var resp = jsonDecode(jsonString.body);
+  print("receive MyPageData 실행");
+  print(resp);
+  print(resp['result']);
+  print(resp['result']['userName']);
+
+
+  print("실행전");
+  MyPageData myPageData=MyPageData(
+        name: resp['result']['userName'],
+        profileImage:resp['result']['profileImgUrl'],
+        level: 'Lv',
+        levelNum: resp['result']['userLevel'],
+        badge1: '1',
+        badge2: '2',
+        badge3: '3',
+      );
+  print("실행 후");
+  print(myPageData);
+
+  return myPageData;
+}
+class MyPageScreen extends StatefulWidget{
+  const MyPageScreen({Key?key}):super(key:key);
+
+  @override
+  State<StatefulWidget> createState() => _MyPageScreen();
+}
+
+class _MyPageScreen extends State<MyPageScreen> {
+  late Future<MyPageData> futureMyPage;
+
+  @override
+  void initState() {
+    futureMyPage=receiveMyPageData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text(
-            "GREENUS",
-            style: GoogleFonts.pacifico(fontSize: 20, color: Colors.white),
-          ),
-        ),
-        body: ListView(
-          children: [
-          MyPageHeader(myPageData: myPageData[0]),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.only(left: 16, bottom: 16),
-              child: Text("진행 중인 미션", style: textTheme().headline1,),
-            ),
-          ],
-        ),
-    );
+    return FutureBuilder<MyPageData>(
+        future: receiveMyPageData(),
+        builder: (context, snapshot) {
+          var data=snapshot.data;
+          if(data==null){
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }else{
+            print("여기가 들어오긴 하니?");
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                title: Text(
+                  "GREENUS",
+                  style: GoogleFonts.pacifico(fontSize: 20, color: Colors.white),
+                ),
+              ),
+              body: ListView(
+                children: [
+                  MyPageHeader(myPageData: data),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16, bottom: 16),
+                    child: Text("진행 중인 미션", style: textTheme().headline1,),
+                  ),
+                ],
+              ),
+            );
+          }
+    });
   }
 }
