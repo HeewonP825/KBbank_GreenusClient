8667a1c4dc9a8719998e67faf628756882f9189f
HeewonP825 <7hwon@naver.com>
2022-09-10 00:10:31 +0900

    make a missionWithFriends page(ongoing)

---
+++ lib/screens/missionPage/components/missionFriendContainer.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/friendList.dart';
+import '../../../theme.dart';
+import '../../components/ImageContainer.dart';
+
+class FriendContainer extends StatelessWidget {
+  const FriendContainer({
+    Key? key,
+    required this.friendList,
+  }) : super(key: key);
+
+  final FriendList friendList;
+
+  @override
+  Widget build(BuildContext context) {
+    print("Friend List build start");
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 0.2,
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.4),
+            spreadRadius: 0.5,
+            blurRadius: 1,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+        color: Colors.  white,
+      ),
+      margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+      height: 96,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            // Checkbox(
+            //   value: this.value,
+            //   onChanged: (bool value) {
+            //     setState(() {
+            //       this.value = value;
+            //     });
+            //   },
+            // ),
+            ImageContainer(
+              width: 55,
+              height: 55,
+              borderRadius: 27.5,
+              imageUrl: friendList.profileImage,
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  Text.rich(
+                    TextSpan(children: [
+                      TextSpan(text: friendList.level),
+                      TextSpan(text: friendList.levelNum.toString()),
+                    ]),
+                  ),
+                  const Spacer(),
+                  Text(
+                    friendList.friendName,
+                    style: textTheme().headline1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const Spacer(),
+                  Text(
+                    friendList.message,
+                    style: textTheme().subtitle1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/missionPage/components/missionWithFriends.dart
+++ lib/screens/missionPage/components/missionWithFriends.dart
@@ -1,36 +1,116 @@
-import 'package:kbbank_practice/screens/challengeDetail/missionStamp.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import '../../../../theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:kbbank_practice/screens/missionPage/components/missionDetail.dart';
+import '../../../models/friendList.dart';
+import '../../friendlist/components/friendContainer.dart';
+import '../../components/ImageContainer.dart';
+import '../../components/appBarPrefferedSize.dart';
+import 'missionDetail.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-import '../../../../theme.dart';
+Future<List<FriendList>> receiveFriendsList() async {
+  var userId = 12;
 
-class MissionWithFriends extends StatelessWidget {
+  var jsonString = await http.get(
+      Uri.parse('https://dev.uksfirstdomain.shop/app/users/${userId}/friends'));
+  var resp = jsonDecode(jsonString.body);
+  print("친구야~!~!@~!@~!@~!놀자~!@~!");
+  print(resp['result']);
+
+  final dataResult=resp['result'];
+  List<FriendList> friendList=[];
+  for(int i=0; i<resp['result'].length;i++){
+
+    friendList.add(FriendList(
+      message: dataResult[i]['statusMessage'],
+      friendName: dataResult[i]['userName'],
+      profileImage: dataResult[i]['profileImgUrl'],
+      levelNum: dataResult[i]['userLevel'],
+      level: 'Lv',
+    ));
+  }
+
+  return friendList;
+}
+
+class MissionWithFriends extends StatefulWidget{
+  const MissionWithFriends({Key?key}):super(key:key);
+
+  @override
+  State<StatefulWidget> createState() => MissionWithFriendsState();
+}
+
+class MissionWithFriendsState extends State<MissionWithFriends>{
+  late Future<List<FriendList>> futureFriends;
+
+  void initState() {
+    super.initState();
+    futureFriends=receiveFriendsList();
+  }
+
+  final ButtonStyle RoundButtonStyle = TextButton.styleFrom(
+    backgroundColor: Colors.green,
+    //primary: Colors.black87,
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    elevation: 5,
+    // shape
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(27.5),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(
-          "GREENUS",
-          style: GoogleFonts.pacifico(fontSize: 20, color: Colors.white),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: (Icon(Icons.keyboard_return)),
-        backgroundColor: Colors.green,//Widget 추가
-        onPressed: () { // 이벤트 콜백 함수
-          Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => MissionDetail()),
-          );
-        },
-      ),
-      body: Column(
+    // final List<FriendList> friendList=receiveFriendsList();
 
-      ),
+    return FutureBuilder<List<FriendList>>(
+        future: futureFriends,
+        builder: (context, snapshot) {
+          var data = snapshot.data;
+          print("친구야`!~@! 잘 놀았다~!@~!2");
+          print(data);
+          if (data == null) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                title: Text(
+                  "GREENUS",
+                  style: GoogleFonts.pacifico(
+                      fontSize: 20, color: Colors.white),
+                ),
+              ),
+              body: ListView(
+                children: List.generate(
+                  snapshot.data?.length ?? 0,
+                      (index) =>
+                      FriendContainer(friendList: snapshot.data![index]),
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                child: (Icon(Icons.keyboard_return)),
+                backgroundColor: Colors.green, //Widget 추가
+                onPressed: () { // 이벤트 콜백 함수
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => MissionDetail()),
+                  );
+                },
+              ),
+            );
+          }
+          // body: Column(
+          //
+          // ),
+        }
     );
   }
 }
